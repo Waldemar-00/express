1a603ccdc2241d7ddb8625af6b205ef9109ed13b
ULADZIMIR <01myit@gmail.com>
2025-04-23 20:17:20 +0300

    External module morgan, express.json() for req.body.

--- middlewares/app.ts
+++ middlewares/app.ts
@@ -1,9 +1,19 @@
 import express from 'express'
-import { logger } from './middleware/logger'
+import morgan from 'morgan'
 const app = express()
 
-app.use(logger)
+app.use(morgan('dev'))
+app.use(express.json())
+//* app.use((req, res, next) => {
+//* 	let dt = ''
+//* 	req.on('data', (chunk) => (dt += chunk))
+//* 	req.on('end', async () => {
+//* 		req.body = await JSON.parse(dt)
+//* 		next()
+//* 	})
+//* })
 app.use((req, res) => {
+	console.log(req.body)
 	res.send(`This is express server port 3000`)
 })
 
