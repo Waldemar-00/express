4265102b6c06e246c1e420d5cc9d5814351b1e37
ULADZIMIR <01myit@gmail.com>
2025-04-24 19:27:24 +0300

    FINISH

--- middlewares/app.ts
+++ middlewares/app.ts
@@ -5,18 +5,6 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-//* app.use((req, res, next) => {
-//* 	if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
-//* 		let dt = ''
-//* 		req.on('data', (chunk) => (dt += chunk.toString()))
-//* 		req.on('end', () => {
-//* 			const object: any = {}
-//* 			const entries = new URLSearchParams(dt)
-//* 			req.body = entries
-//* 			next()
-//* 		})
-//* 	} else next()
-//* })
 app.use((req, res) => {
 	console.log(req.body)
 	res.send(`This is express server port 3000`)
